c7a61fd7f957c34df781401955de239d9ad2b8c9
scsfdev <25923486+scsfdev@users.noreply.github.com>
2021-04-07 14:54:15 +0800

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMethod1 = new System.Windows.Forms.Button();
+            this.btnMethod2 = new System.Windows.Forms.Button();
+            this.txtDB = new System.Windows.Forms.TextBox();
+            this.txtCSV = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMethod1 = new System.Windows.Forms.Label();
+            this.lblMethod2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMethod1
+            // 
+            this.btnMethod1.Location = new System.Drawing.Point(43, 148);
+            this.btnMethod1.Name = "btnMethod1";
+            this.btnMethod1.Size = new System.Drawing.Size(75, 23);
+            this.btnMethod1.TabIndex = 0;
+            this.btnMethod1.Text = "Method 1";
+            this.btnMethod1.UseVisualStyleBackColor = true;
+            this.btnMethod1.Click += new System.EventHandler(this.btnMethod1_Click);
+            // 
+            // btnMethod2
+            // 
+            this.btnMethod2.Location = new System.Drawing.Point(43, 194);
+            this.btnMethod2.Name = "btnMethod2";
+            this.btnMethod2.Size = new System.Drawing.Size(75, 23);
+            this.btnMethod2.TabIndex = 1;
+            this.btnMethod2.Text = "Method 2";
+            this.btnMethod2.UseVisualStyleBackColor = true;
+            this.btnMethod2.Click += new System.EventHandler(this.btnMethod2_Click);
+            // 
+            // txtDB
+            // 
+            this.txtDB.Location = new System.Drawing.Point(43, 42);
+            this.txtDB.Name = "txtDB";
+            this.txtDB.Size = new System.Drawing.Size(312, 20);
+            this.txtDB.TabIndex = 2;
+            // 
+            // txtCSV
+            // 
+            this.txtCSV.Location = new System.Drawing.Point(43, 97);
+            this.txtCSV.Name = "txtCSV";
+            this.txtCSV.Size = new System.Drawing.Size(312, 20);
+            this.txtCSV.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "DB:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "CSV:";
+            // 
+            // lblMethod1
+            // 
+            this.lblMethod1.AutoSize = true;
+            this.lblMethod1.Location = new System.Drawing.Point(142, 153);
+            this.lblMethod1.Name = "lblMethod1";
+            this.lblMethod1.Size = new System.Drawing.Size(10, 13);
+            this.lblMethod1.TabIndex = 6;
+            this.lblMethod1.Text = "-";
+            // 
+            // lblMethod2
+            // 
+            this.lblMethod2.AutoSize = true;
+            this.lblMethod2.Location = new System.Drawing.Point(142, 199);
+            this.lblMethod2.Name = "lblMethod2";
+            this.lblMethod2.Size = new System.Drawing.Size(10, 13);
+            this.lblMethod2.TabIndex = 7;
+            this.lblMethod2.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 242);
+            this.Controls.Add(this.lblMethod2);
+            this.Controls.Add(this.lblMethod1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCSV);
+            this.Controls.Add(this.txtDB);
+            this.Controls.Add(this.btnMethod2);
+            this.Controls.Add(this.btnMethod1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMethod1;
+        private System.Windows.Forms.Button btnMethod2;
+        private System.Windows.Forms.TextBox txtDB;
+        private System.Windows.Forms.TextBox txtCSV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMethod1;
+        private System.Windows.Forms.Label lblMethod2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SQLite;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        SQLiteConnection sql_con;
+
+        class Item
+        {
+            public Int64 col1 { get; set; }
+            public int col2 { get; set; }
+            public string col3 { get; set; }
+            public int col4 { get; set; }
+            public int col5 { get; set; }
+            public int col6 { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtDB.Text = @"C:\Software\SampleDB.db";
+            txtCSV.Text = @"C:\Software\wmitems.txt";
+
+            SetConnection();
+        }
+
+        private void SetConnection()
+        {
+            sql_con = new SQLiteConnection
+                (@"Data Source=" + txtDB.Text + ";Version=3;New=False;Compress=True;");
+
+        }
+
+        int ToInt32(object obj)
+        {
+            int iOut = 0;
+            if (obj is DBNull || obj == null)
+                return 0;
+
+            if (int.TryParse(obj.ToString().Trim(), out iOut))
+                return iOut;
+            else
+                return 0;
+        }
+
+        Int64 ToInt64(object obj)
+        {
+            if (obj is DBNull || obj == null)
+                return 0;
+
+            Int64 iOut64 = Convert.ToInt64(obj.ToString().Trim());
+
+            return iOut64;
+        }
+
+        private void btnMethod1_Click(object sender, EventArgs e)
+        {
+            lblMethod1.Text = "Start Time: " + DateTime.Now.ToString("hh:mm:ss");
+            Method1();
+            lblMethod1.Text += ". End Time: " + DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        private void Method1()
+        {
+            try
+            {
+                sql_con.Open();
+                Console.WriteLine("Start: " + DateTime.Now);
+                var lines = File.ReadAllLines(txtCSV.Text);
+                foreach (var line in lines)
+                {
+                    Item i = new Item();
+                    i.col1 = ToInt64(line.Substring(0, 15));
+                    i.col2 = ToInt32(line.Substring(15, 9));
+                    i.col3 = line.Substring(24, 35).Trim().Replace("\"", "");
+                    i.col4 = ToInt32(line.Substring(59, 10));
+                    i.col5 = ToInt32(line.Substring(69, 10));
+                    i.col6 = ToInt32(line.Substring(79, 5));
+
+                    Insert(i);
+                }
+
+                Console.WriteLine("End: " + DateTime.Now);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            finally
+            {
+                sql_con.Close();
+            }
+        }
+
+        
+        void Insert(Item i)
+        {
+            
+            SQLiteCommand insertSQL = new SQLiteCommand("INSERT INTO master (col1,col2,col3,col4,col5,col6) VALUES (?,?,?,?,?,?)", sql_con);
+            insertSQL.Parameters.AddWithValue("col1",i.col1);
+            insertSQL.Parameters.AddWithValue("col2", i.col2);
+            insertSQL.Parameters.AddWithValue("col3", i.col3);
+            insertSQL.Parameters.AddWithValue("col4", i.col4);
+            insertSQL.Parameters.AddWithValue("col5", i.col5);
+            insertSQL.Parameters.AddWithValue("col6", i.col6);
+
+            try
+            {
+                insertSQL.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            
+        }
+
+
+        
+
+        private void btnMethod2_Click(object sender, EventArgs e)
+        {
+            lblMethod2.Text = "Start Time: " + DateTime.Now.ToString("hh:mm:ss");
+            Method2();
+            lblMethod2.Text += ". End Time: " + DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        private void Method2()
+        {
+            sql_con.Open();
+            SQLiteCommand command = sql_con.CreateCommand();
+            SQLiteTransaction transaction = sql_con.BeginTransaction();
+
+            command.CommandText = "INSERT INTO Master "
+                                    + "(col1, col2, col3, col4, col5, col6) " +
+                                    "VALUES (@col1, @col2, @col3, @col4, @col5, @col6)";
+
+            command.Parameters.AddWithValue("@col1", "");
+            command.Parameters.AddWithValue("@col2", "");
+            command.Parameters.AddWithValue("@col3", "");
+            command.Parameters.AddWithValue("@col4", "");
+            command.Parameters.AddWithValue("@col5", "");
+            command.Parameters.AddWithValue("@col6", "");
+
+            var lines = File.ReadAllLines(txtCSV.Text);
+            foreach (var line in lines)
+            {
+                InsertResultItem(ToInt64(line.Substring(0, 15)), ToInt32(line.Substring(15, 9)), line.Substring(24, 35).Trim().Replace("\"", ""),
+                                 ToInt32(line.Substring(59, 10)), ToInt32(line.Substring(69, 10)), ToInt32(line.Substring(79, 5)), command);
+            }
+
+            transaction.Commit();
+            command.Dispose();
+         //   sql_con.Dispose();
+        }
+
+        private int InsertResultItem(Int64 col1, int col2, string col3, int col4, int col5, int col6, SQLiteCommand command)
+        {
+            command.Parameters["@col1"].Value = col1;
+            command.Parameters["@col2"].Value = col2;
+            command.Parameters["@col3"].Value = col3;
+            command.Parameters["@col4"].Value = col4;
+            command.Parameters["@col5"].Value = col5;
+            command.Parameters["@col6"].Value = col6;
+            return command.ExecuteNonQuery();
+        }
+    }
+}
